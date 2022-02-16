1d5eaf411b23676f23257900290896f04b5bd8f3
command <rekreb81@gmail.com>
2022-02-16 20:52:58 +0300

    init7 add menu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import tkinter
-from tkinter import Tk, Label, Entry, Button, END, ttk
+from tkinter import Tk, Label, Entry, Button, END, ttk, Menu
 from tkinter import messagebox
 from scipy.interpolate import interp1d
 
@@ -18,8 +17,8 @@ def main():
 
 
 def hesap(arg: str):
-    x = xinput1.get().split(",")
-    y = xinput2.get().split(",")
+    x = Xinput1.get().split(",")
+    y = Xinput2.get().split(",")
     x1 = []
     y1 = []
     for xx in x:
@@ -29,14 +28,14 @@ def hesap(arg: str):
     if len(x1) != len(y1):
         messagebox.showinfo(title="uyarı", message="diziler birbirine eşit değil.")
     else:
-        z = xinput3.get()
+        z = Xinput3.get()
         if arg == "x":
             intp = interp1d(x1, y1, fill_value="extrapolate")
         else:
             intp = interp1d(y1, x1, fill_value="extrapolate")
         sonuc = intp(z)
-        xinput4.delete(0, END)
-        xinput4.insert(0, str(sonuc))
+        Xinput4.delete(0, END)
+        Xinput4.insert(0, str(sonuc))
 
 
 def destroy_me():
@@ -56,7 +55,7 @@ Pencere.title("İnterpolasyon Hesaplama")
 Pencere.resizable(False, False)
 Pencere.protocol('WM_DELETE_WINDOW', destroy_me)
 
-menubar = tkinter.Menu(Pencere)
+menubar = Menu(Pencere)
 Pencere.configure(menu=menubar)
 menubar.add_command(label="Hakkında", command=hakkinda)
 menubar.add_command(label="Çıkış", command=destroy_me)
@@ -73,25 +72,25 @@ Etiket3.place(x=10, y=90)
 Etiket4 = Label(Pencere, text="Sonuc: ")
 Etiket4.place(x=180, y=92)
 
-xinput1 = Entry(Pencere, width=80)
-xinput1.place(x=30, y=9)
+Xinput1 = Entry(Pencere, width=80)
+Xinput1.place(x=30, y=9)
 
-xinput2 = Entry(Pencere, width=80)
-xinput2.place(x=30, y=49)
+Xinput2 = Entry(Pencere, width=80)
+Xinput2.place(x=30, y=49)
 
-xinput3 = Entry(Pencere)
-xinput3.place(x=30, y=90)
+Xinput3 = Entry(Pencere)
+Xinput3.place(x=30, y=90)
 
-xinput4 = Entry(Pencere)
-xinput4.place(x=230, y=90)
+Xinput4 = Entry(Pencere)
+Xinput4.place(x=230, y=90)
 
-xbuton2 = Button(Pencere, text="X", command=lambda: hesap("x"))
-xbuton2.bind_all("x", lambda x: hesap("x"))
-xbuton2.place(x=30, y=130)
+Xbuton2 = Button(Pencere, text="X", command=lambda: hesap("x"))
+Xbuton2.bind_all("x", lambda x: hesap("x"))
+Xbuton2.place(x=30, y=130)
 
-xbuton3 = Button(Pencere, text="Y", command=lambda: hesap("y"))
-xbuton3.bind_all("y", lambda x: hesap("y"))
-xbuton3.place(x=70, y=130)
+Xbuton3 = Button(Pencere, text="Y", command=lambda: hesap("y"))
+Xbuton3.bind_all("y", lambda x: hesap("y"))
+Xbuton3.place(x=70, y=130)
 
 # ttk içinde "from tkinter import ttk"
 TSeparator1 = ttk.Separator(Pencere)
